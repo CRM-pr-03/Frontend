db2c37b8c867e7e7241b347b21172c68200c224a
CRM-pr-03 <161812804+CRM-pr-03@users.noreply.github.com>
2024-03-07 18:32:36 +0530

    Add files via upload

---
+++ Login/login.component.ts
@@ -0,0 +1,30 @@
+// login.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Implement your login logic here
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      console.log('Login clicked. Username:', username, 'Password:', password);
+    }
+  }
+}
